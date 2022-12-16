51df2558f1f78ca201235bf1a63837a46d6648d3
oscarcollerde <fatihhusnub@gmail.com>
2022-12-16 22:36:11 +0300

    dark-mode
    click the button and change your website mode from light to dark

---
+++ dark mode/app.js
@@ -0,0 +1,19 @@
+
+const darkButton = document.querySelector(".dark-container");
+const body=document.querySelector("body");
+const darkIcon=document.querySelector(".fa-solid");
+const i=document.querySelector("i");
+
+darkButton.addEventListener("click", ()=>{
+    
+    darkButton.classList.toggle("dark");
+    if(darkButton.classList.contains("dark")){        
+        body.style = `background-color:#222831; color:#f0f0f0`;
+        darkButton.style.backgroundColor=`#f0f0f0`;
+        darkIcon.style.color=`#222831`;
+    }else{        
+        body.style=`background-color:#fdfdfd; color:black`;
+        darkButton.style.backgroundColor=`#222831`;
+        darkIcon.style.color=`#f0f0f0`;
+    }
+})
